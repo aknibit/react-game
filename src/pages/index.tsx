cb8a9f2f75e5aff18806402c3e45885718957142
Jonathan Rodz <jonathan@aknibit.com>
2025-03-13 18:53:57 +0100

    basic layout and prettier dependency

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,113 +1,78 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import { useState } from "react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+enum Player {
+  Red = "red",
+  Blue = "blue",
+}
 
 export default function Home() {
+  const [cols, setCols] = useState(10);
+  const [nextPlayer, setNextPlayer] = useState<Player>();
+
+  const startGame = () => {
+    if (nextPlayer) {
+      setNextPlayer(undefined);
+    } else {
+      setNextPlayer(Math.random() > 0.5 ? Player.Red : Player.Blue);
+    }
+  };
+
+  const executeMove = (col: number) => {
+    console.log(nextPlayer, "clicked", col);
+    const nextOne = nextPlayer === Player.Red ? Player.Blue : Player.Red;
+    setNextPlayer(nextOne);
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="bg-gray-900 grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <section className="flex gap-2 items-center sm:items-start">
+        <input
+          className="bg-gray-700 p-2"
+          hidden={nextPlayer ? true : false}
+          type="number"
+          placeholder="cols"
+          value={cols}
+          onChange={(ev) => {
+            setCols(Number(ev.target.value));
+          }}
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <button className="bg-green-800 py-2 px-4" onClick={startGame}>
+          {nextPlayer ? "RESTART" : "START"}
+        </button>
+      </section>
+
+      <section className="center">
+        <div className="flex gap-1">
+          {nextPlayer &&
+            Array.from({ length: cols }).map((_, i) => (
+              <div
+                key={i}
+                className="bg-gray-800 h-12 w-12 flex items-center justify-center text-white"
+                onClick={() => {
+                  executeMove(i);
+                }}
+              ></div>
+            ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
+
+      <section>
+        {nextPlayer && (
+          <div
+            className={`${nextPlayer === "red" ? "bg-red-800/30" : "bg-blue-800/30"} py-2 px-4`}
+          >
+            <p>
+              Turn for{" "}
+              <span
+                className={`${nextPlayer === "red" ? "text-red-400" : "text-blue-400"} font-bold`}
+              >
+                {nextPlayer}{" "}
+              </span>
+              team
+            </p>
+          </div>
+        )}
+      </section>
     </div>
   );
 }
